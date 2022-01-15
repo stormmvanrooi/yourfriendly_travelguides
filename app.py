d64b40517cffd44fecfda034f38c15466f8ebd14
Stormm Van Rooi <stormm.vanrooi@gmail.com>
2022-01-15 18:40:51 -0500

    Adding updated dashboard

--- app.py
+++ app.py
@@ -1,25 +1,68 @@
-import json
-from flask import Flask, render_template, jsonify, request
-
-# create instance of Flask app
+from flask import Flask, render_template, redirect, jsonify
+import pymongo
 app = Flask(__name__)
+from pymongo import MongoClient
+import json
 
-# Data Store
-with open("test.json") as fp:
-    search_results = json.load(fp)
+conn = "mongodb://localhost:27017"
+client = pymongo.MongoClient(conn)
+db = client.NYC
+
+restaurants_collection = db.restaurant_data
+hotels_collection = db.hotel_data
+museums_collection = db.museums_data
+groceries_collection = db.groceries_data
+shopping_collection = db.shopping_data
+
+with open("hotels_data.json") as fp:
+    hotels = json.load(fp)
+
+with open("restaurants_data.json") as fp:
+    restaurants = json.load(fp)
+
+with open("museums_data.json") as fp:
+    museums = json.load(fp)
+
+with open("shopping_data.json") as fp:
+    shopping = json.load(fp)
+
+with open("groceries_data.json") as fp:
+    groceries = json.load(fp)
 
 # ========================================
 # Flask as Data Presentation
 # ========================================
 
-# create route that renders index.html template
 @app.route("/")
-def index():
-    return render_template("index.html", searchResults=search_results)
+def data():
+    rest_res = list(restaurants_collection.find({},{'_id':0}))
+    hotel_res = list(hotels_collection.find({},{'_id':0}))
+    museum_res = list(museums_collection.find({},{'_id':0}))
+    groceries_res = list(groceries_collection.find({},{'_id':0}))
+    shopping_res = list(shopping_collection.find({},{'_id':0}))
+    #return(str(shopping_res[0]))
 
-@app.route("/api")
-def api():
-    return jsonify(search_results)
+    return render_template("index.html",htmlRestaurant = rest_res, htmlHotel = hotel_res, htmlMuseum = museum_res, htmlGroceries = groceries_res, htmlShopping = shopping_res )
 
-if __name__ =="__main__":
-    app.run(debug=True, port=5005)
+@app.route("/api/hotels")   
+def hotel():
+    return jsonify(hotels)
+
+@app.route("/api/restaurants")   
+def restaurant():
+    return jsonify(restaurants)
+
+@app.route("/api/museums")   
+def museum():
+    return jsonify(museums)
+
+@app.route("/api/shopping")   
+def shop():
+    return jsonify(shopping)
+
+@app.route("/api/groceries")   
+def grocery():
+    return jsonify(groceries)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,39 +1,68 @@
+let plotData = [];
 let hotelNames = [];
-let hotelRatings = [];
-
-d3.json("http://127.0.0.1:5005/api").then(function (response) {
-    for (let index = 0; index < response.length; index++) {
-        let restaurant = response[index];
-        hotelNames.push(restaurant.Name);
-        hotelRatings.push(restaurant.Rating);
+let hotelDistance = [];
+function getData(respHotel) {
+    let parsingData = respHotel;
+    for (let index = 0; index < parsingData.length; index++) {
+        let hotel_data = parsingData[index];
+        plotData.push(
+            {
+                "name": hotel_data.name,
+                "dist": hotel_data.distance
+            }
+        )
+        //console.log(hotel_data)
     }
-
+    // Sort data in descending order
+    let descData = plotData.sort(function sortFunction(a,b){
+        return a.dist - b.dist;
+    }).slice(0,10);
     // Trace1 for the Greek Data
+    console.log(descData)
+    // Create traceBar for horizontal bar graph
+    let colors = ['#4169e1', '#324ab2', '#2a52be',  '#0047ab', '#0038a8', '#003399', '#041690', '#002387', '#002366', '#002147']
     let trace1 = {
-        x: hotelNames,
-        y: hotelRatings,
-        name: "New York City Hotel Ratings",
+        x: descData.map(i => i.dist),
+        y: plotData.map(i => i.name),
+        text: plotData.map(i => i.name),
         type: "bar",
-        orientation: "h"
+        orientation: "h",
+        mode: 'markers',
+        marker: {
+                color: colors
+        }
     };
-
     // Data array
     // `data` has already been defined, so we must choose a new name here:
     let traceData = [trace1];
-
     // Apply a title to the layout
     let layout = {
-        title: "New York Hotels",
+        xaxis: {autorange: true, 
+            title: 'Distance in meters from Manhattan',},
+        showticklabels: true,
+        yaxis:{
+        tickangle: 0},
+        tickfont: {
+            family: 'Old Standard TT, serif',
+            size: 12,
+        color: 'black'
+        },
+            width: 750,
+            height: 400,
+            plot_bgcolor: 'rgb(248,248,255)',
         margin: {
-            l: 100,
-            r: 100,
-            t: 100,
-            b: 100
+            l: 250,
+            r: 50,
+            b: 50,
+            t: 50,
+            pad: 4
         }
     };
 
     // Render the plot to the div tag with id "plot"
     // Note that we use `traceData` here, not `data`
-    Plotly.newPlot("plot-id", traceData, layout);
 
-})
+    Plotly.newPlot("plot-id", traceData, layout, {responsive:true});
+console.log(traceData)
+}
+getData(respHotel)
